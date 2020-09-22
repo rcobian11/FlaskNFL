2d2d18fb5c0ec76fb24c5f7313b9036f0fbecd5c
rcobian11 <rcobian29@yahoo.com>
2020-09-22 13:02:05 -0700

    Added form validation

--- NFLpicks.py
+++ NFLpicks.py
@@ -1,31 +1,28 @@
 from flask import Flask, redirect, url_for, request, render_template
-import csv, argparse
+import argparse
+import helper, scrapper
 app = Flask(__name__)
-PICKS = []
+
+PICKS = helper.gen_nflpick()
+config_len = helper.file_len("config.csv")
 
 @app.route('/')
 def picks():
-	return render_template('ScriptPicks.html', picks=PICKS)
+	return render_template('ScriptPicks.html', picks=PICKS, ctr=range(1,config_len+1))
 
 @app.route('/submit', methods = ['POST', 'GET'])
 def submit():
 	picks = []
-	'''
 	if request.method == 'POST':
 		user = request.form['name']
-		for ctr in range(1,17):
+		for ctr in range(1,config_len+1):
 			pick = request.form['pick' + str(ctr)]
 			picks.append(pick)
 		points = request.form['points']
 		print(picks)
 		return "done"
-	'''
 	return 'done'
 if __name__ == '__main__':
-	#TODO create python script that creates html form
-	#TODO create python script that creates js file for validation
-	with open('config.csv', newline = "") as csvfile:
-		reader = csv.DictReader(csvfile, fieldnames = ['fav', 'spread', 'under'])
-		for row in reader:
-			PICKS.append((row['fav'], row['spread'], row['under']))
+	#TODO get values from form and put them in a csv file
+	#incorporate scrapper.py using argparse
 	app.run(debug = True)
---
+++ helper.py
@@ -0,0 +1,23 @@
+import csv
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    f.close()
+    return i + 1
+
+def gen_nflpick():
+	picks = []
+	nflpicks = open("picks.csv", 'w')
+	with open('config.csv', newline = '') as csvfile:
+		reader = csv.DictReader(csvfile, fieldnames = ['FAV', 'SPREAD', 'UNDER'])
+		pickNum = 1
+		nflpicks.write(",")
+		for row in reader:
+			picks.append((row['FAV'], row['SPREAD'], row['UNDER']))
+			nflpicks.write('"{}\n{}\n{}",'.format(row['FAV'],
+												row['SPREAD'],
+												row['UNDER']))
+		nflpicks.write('"TIE\n-\nBRK"')
+	nflpicks.close()
+	return(picks)
