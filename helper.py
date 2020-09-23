e6cd56df7c9dbd63db53875921212fafb4a913c7
ricardo <rcobian29@yaho.com>
2020-09-23 01:45:27 -0700

    try2

--- helper.py
+++ helper.py
@@ -8,7 +8,7 @@ def file_len(fname):
 
 def gen_nflpick():
 	picks = []
-	nflpicks = open("/home/ec2-user/picks.csv", 'w')
+	nflpicks = open("~/home/ec2-user/picks.csv", 'w')
 	with open('config.csv', newline = '') as csvfile:
 		reader = csv.DictReader(csvfile, fieldnames = ['FAV', 'SPREAD', 'UNDER'])
 		pickNum = 1
@@ -23,7 +23,7 @@ def gen_nflpick():
 	return(picks)
 
 def submit_picks(name, picks, points):
-	file = open("/home/ec2-user/picks.csv", 'a')
+	file = open("~/home/ec2-user/picks.csv", 'a')
 	file.write("\n")
 	file.write(name + ",")
 	for pick in picks:
